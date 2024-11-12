eac303c2a90e21755a60e6da20cfac4f01aca8ec
LoveDero81 <153917563+LoveDero81@users.noreply.github.com>
2024-11-12 13:54:51 +0400

    upd3

---
+++ src/scripts/modules/utils.js
@@ -0,0 +1,7 @@
+export function utils(){
+    let burger=document.querySelector('.header-section__info-block__item__burger-logo__burger ')
+    let body=document.querySelector('body')
+    burger.addEventListener('click',function(event){
+        body.classList.toggle('test')
+    })
+}
