9388383bff947f3070fb3c034c90ed08113559af
Boris Karelin <thetomasselden@gmail.com>
2024-11-25 23:40:23 +0400

    add folder docs and small fixes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { accordion } from './js/modules/accordion.js';
 import { Slider, BannSlider, BrandsSlider } from './js/modules/swiper/slider.js';
 import { utils } from './js/modules/utils.js';
+import "./styles/main.scss"
 
 
 accordion()
